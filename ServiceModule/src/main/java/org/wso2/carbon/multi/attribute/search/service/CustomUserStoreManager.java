dfc7ee6455e711e68bffb73d0f694f3647b1562e
inshaf <inshaf.ucsc@gmail.com>
2016-07-08 17:41:01 +0530

    Adding claim filter method
    Adding claim filtering support for TFL

--- ServiceModule/src/main/java/org/wso2/carbon/multi/attribute/search/service/CustomUserStoreManager.java
+++ ServiceModule/src/main/java/org/wso2/carbon/multi/attribute/search/service/CustomUserStoreManager.java
@@ -21,6 +21,8 @@ public class CustomUserStoreManager {
 
 
     private static Log log = LogFactory.getLog(CustomUserStoreManager.class);
+    private static String filter = "*";
+    private static int maxItemLimit = -1;
     RealmConfiguration realmConfig;
 
     public CustomUserStoreManager() throws UserStoreException {
@@ -33,87 +35,6 @@ public class CustomUserStoreManager {
             throw new UserStoreException(
                     "Failed to initiate User Realm from Carbon Context");
         }
-    }
-
-
-    public UserListAttributes getMultiAttributeObject(String[] claims, String[] claimValues, String[] profileNames, int startAt, int endAt) throws UserStoreException {
-
-        String[] allUsers = null;
-
-        if (claims.length != claimValues.length || claims.length != profileNames.length) {
-            throw new UserStoreException(
-                    "Claims, claim values and profile names consist of invalid length");
-        } else if (startAt < 0 || endAt < 0) {
-            throw new UserStoreException(
-                    "Missing startAt or endAt values");
-        } else {
-
-            if (claims == null) {
-                claims = new String[0];
-            }
-
-            if (claimValues == null) {
-                claimValues = new String[0];
-            }
-
-
-            int claimListLen = claims.length;
-            for (int i = 0; i < claimListLen; i++) {
-                if (log.isDebugEnabled()) {
-                    log.debug("Claim Names: " + claims[i] + " Claim value: " + claimValues[i]);
-                }
-
-                try {
-                    RealmService realmService = MultiSearchComponentServiceHolder.getRealmService();
-
-                    String[] userArray;
-
-                    if ("null".equals(profileNames[i]) || "".equals(profileNames[i])) {
-                        userArray = realmService.getUserRealm(realmConfig).getUserStoreManager().getUserList(claims[i], claimValues[i], null);
-                    } else {
-                        userArray = realmService.getUserRealm(realmConfig).getUserStoreManager().getUserList(claims[i], claimValues[i], profileNames[i]);
-                    }
-
-                    if (userArray.length == 0) {
-                        allUsers = null;
-                        break;
-                    } else {
-                        if (allUsers == null) {
-                            allUsers = userArray;
-                        } else {
-                            allUsers = Helper.retainCommonWithBase(allUsers, userArray);
-                        }
-
-                    }
-
-
-                    if (log.isDebugEnabled()) {
-                        log.debug("UserList: " + Arrays.toString(allUsers));
-                    }
-
-
-                } catch (UserStoreException e) {
-                    throw new UserStoreException(
-                            "Error occurred while getting user list for. " + "Claim Name: " + claims[i] + " Claim value: " + claimValues[i]);
-                }
-
-
-            }
-        }
-
-        if (allUsers.length < endAt) {
-            Arrays.copyOfRange(allUsers, startAt, allUsers.length);
-        } else {
-            Arrays.copyOfRange(allUsers, startAt, endAt);
-        }
-
-        UserListAttributes userListAttr = new UserListAttributes();
-        userListAttr.setStartAt(startAt);
-        userListAttr.setMaxResults(endAt);
-        userListAttr.setTotal(allUsers.length);
-        userListAttr.setUserList(allUsers);
-
-        return userListAttr;
     }
 
 
@@ -187,20 +108,16 @@ public class CustomUserStoreManager {
         return allUsers;
     }
 
-
-    public UserListAttributes getMultiAttributeObjectWithFilter(String[] claims, String[] claimValues, String[] profileNames, String[] filterClaims, String[] filterClaimValues, String[] filterProfileNames, int startAt, int endAt) throws UserStoreException {
+    public UserListAttributes getMultiAttributeObject(String[] claims, String[] claimValues, String[] profileNames, int startAt, int maxResults) throws UserStoreException {
 
         String[] allUsers = null;
 
         if (claims.length != claimValues.length || claims.length != profileNames.length) {
             throw new UserStoreException(
                     "Claims, claim values and profile names consist of invalid length");
-        } else if (filterClaims.length != filterClaimValues.length || filterClaims.length != filterProfileNames.length) {
+        } else if (startAt < 0 || maxResults < 0) {
             throw new UserStoreException(
-                    "Filter Claims, claim values and profile names consist of invalid length");
-        } else if (startAt < 0 || endAt < 0) {
-            throw new UserStoreException(
-                    "Missing startAt or endAt values");
+                    "Missing startAt or maxResults values");
         } else {
 
             if (claims == null) {
@@ -211,9 +128,57 @@ public class CustomUserStoreManager {
                 claimValues = new String[0];
             }
 
-            if (profileNames == null) {
-                profileNames = new String[0];
+            if (claimValues == null) {
+                claimValues = new String[0];
             }
+
+
+            try {
+                allUsers = getMultiAttributeUserList(claims, claimValues, profileNames);
+
+                if (log.isDebugEnabled()) {
+                    log.debug("UserList: " + Arrays.toString(allUsers));
+                }
+
+
+            } catch (UserStoreException e) {
+                throw new UserStoreException(e.getMessage());
+            }
+
+
+        }
+
+        int total = allUsers.length;
+
+        if (total<(startAt+maxResults)){
+            allUsers = Arrays.copyOfRange(allUsers, startAt, total);
+        }else{
+            allUsers = Arrays.copyOfRange(allUsers, startAt, (startAt+maxResults));
+        }
+
+
+        UserListAttributes userListAttr = new UserListAttributes();
+        userListAttr.setStartAt(startAt);
+        userListAttr.setMaxResults(maxResults);
+        userListAttr.setTotal(total);
+        userListAttr.setUserList(allUsers);
+
+        return userListAttr;
+    }
+
+
+    public UserListAttributes getMultiAttributeObjectWithFilter(String[] filterClaims, String[] filterClaimValues, String[] filterProfileNames, int startAt, int maxResults) throws UserStoreException {
+
+        String[] allUsers = null;
+
+        if (filterClaims.length != filterClaimValues.length || filterClaims.length != filterProfileNames.length) {
+            throw new UserStoreException(
+                    "Filter Claims, claim values and profile names consist of invalid length");
+        } else if (startAt < 0 || maxResults < 0) {
+            throw new UserStoreException(
+                    "Missing startAt or endAt values");
+        } else {
+
             if (filterClaims == null) {
                 filterClaims = new String[0];
             }
@@ -229,33 +194,58 @@ public class CustomUserStoreManager {
 
             try {
 
-                String[] userArray = getMultiAttributeUserList(claims, claimValues, profileNames);
+                String[] userArray = getUserList();
 
                 String[] filterUserArray = getMultiAttributeUserList(filterClaims, filterClaimValues, filterProfileNames);
 
                 allUsers = Helper.removeCommonWithBase(userArray, filterUserArray);
 
             } catch (UserStoreException e) {
-                throw new UserStoreException(
-                        "Error occurred while getting user list");
+                throw new UserStoreException(e.getMessage());
             }
 
 
         }
 
-        if (allUsers.length < endAt) {
-            Arrays.copyOfRange(allUsers, startAt, allUsers.length);
-        } else {
-            Arrays.copyOfRange(allUsers, startAt, endAt);
+
+        int total = allUsers.length;
+
+        if (total<(startAt+maxResults)){
+            allUsers = Arrays.copyOfRange(allUsers, startAt, total);
+        }else{
+            allUsers = Arrays.copyOfRange(allUsers, startAt, (startAt+maxResults));
         }
 
         UserListAttributes userListAttr = new UserListAttributes();
         userListAttr.setStartAt(startAt);
-        userListAttr.setMaxResults(endAt);
-        userListAttr.setTotal(allUsers.length);
+        userListAttr.setMaxResults(maxResults);
+        userListAttr.setTotal(total);
         userListAttr.setUserList(allUsers);
 
         return userListAttr;
+    }
+
+
+    public String[] getUserList() throws UserStoreException {
+
+        String[] allUsers = null;
+
+        try {
+            RealmService realmService = MultiSearchComponentServiceHolder.getRealmService();
+
+            allUsers = realmService.getUserRealm(realmConfig).getUserStoreManager().listUsers(filter, maxItemLimit);
+
+            if (log.isDebugEnabled()) {
+                log.debug("UserList: " + Arrays.toString(allUsers));
+            }
+
+
+        } catch (UserStoreException e) {
+            throw new UserStoreException(
+                    "Error occurred while getting user list");
+        }
+
+        return allUsers;
     }
 
 }
